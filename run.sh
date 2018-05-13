1c21aa919964e06806922b08302da0426ec046a7
Horst Schneider <horst.schneider@posteo.de>
2018-05-13 20:22:07 +0200

    Add apoc and an example query for nested json

--- run.sh
+++ run.sh
@@ -9,6 +9,7 @@ docker run -d \
     --publish=7474:7474 \
     --publish=7687:7687 \
     --volume=$(pwd)/conf:/conf \
+    --volume=$(pwd)/plugins:/plugins \
     --volume=$(pwd)/logs:/logs \
     --volume=$(pwd)/import:/var/lib/neo4j/import \
     neo4j:3.3.2
